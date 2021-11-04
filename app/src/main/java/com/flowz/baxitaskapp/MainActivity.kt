5838053880b4153e84b28d0010a5d06dbf14225f
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-11-03 23:46:17 -0700

    picture from dribble edited to create baxi splashscreen

--- app/src/main/java/com/flowz/baxitaskapp/MainActivity.kt
+++ app/src/main/java/com/flowz/baxitaskapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_BaxiTaskApp)
         setContentView(R.layout.activity_main)
 
         navController = findNavController( R.id.nav_host_fragment)
